a5789cec77af72dd66299ccd4edecbd346ceb418
Rida R <58844180+rida-re@users.noreply.github.com>
2025-11-08 09:30:58 +0100

    Update deploy-sap-commerce.sh

--- .github/deploy-sap-commerce.sh
+++ .github/deploy-sap-commerce.sh
@@ -1,43 +1,42 @@
-[bash]#!/bin/bash
+#!/bin/bash
 
-create_deployment_output=$(curl -K "./curl-config.txt" \
+create_deployment_output=$(curl -K "./.github/curl-config.txt" \
   -X POST "https://portalapi.commerce.ondemand.com/v2/subscriptions/$SUBSCRIPTION_CODE/deployments" \
   --header "Authorization: Bearer $API_TOKEN" \
   --data "{\"buildCode\":\"$1\",\"databaseUpdateMode\":\"UPDATE\", \"environmentCode\": \"$2\", \"strategy\": \"ROLLING_UPDATE\"}")
 
-# Check if the command succeeded
+# Vérifie si la commande a réussi
 if [ $? -ne 0 ]; then
   echo "$create_deployment_output" | jq .
   exit 1
 fi
 
-code=$(echo $create_deployment_output | jq -r .code)
+code=$(echo "$create_deployment_output" | jq -r .code)
 echo "Successfully created deployment:"
-echo $create_deployment_output | jq .
+echo "$create_deployment_output" | jq .
 
 counter=0
-status=SCHEDULED
+status="SCHEDULED"
 
 while [[ $counter -lt 100 ]] && [[ "$status" == "SCHEDULED" || "$status" == "DEPLOYING" ]]; do
   let counter=counter+1 
 
-  deployment_progress_output=$(curl -K "./scripts/curl-config.txt" \
+  deployment_progress_output=$(curl -K "./.github/curl-config.txt" \
     --header "Authorization: Bearer $API_TOKEN" \
     "https://portalapi.commerce.ondemand.com/v2/subscriptions/$SUBSCRIPTION_CODE/deployments/$code/progress")
     
-  # Check if the command succeeded
   if [ $? -ne 0 ]; then
     echo "$deployment_progress_output" | jq .
     exit 1
   fi
 
-  status=$(echo $deployment_progress_output | jq -r .deploymentStatus)
-  percentage=$(echo $deployment_progress_output | jq -r .percentage)
+  status=$(echo "$deployment_progress_output" | jq -r .deploymentStatus)
+  percentage=$(echo "$deployment_progress_output" | jq -r .percentage)
   echo "$status $percentage%"
 
   if [[ "$status" != "SCHEDULED" && "$status" != "DEPLOYING" ]]; then
-    echo "build has reached end state: $status"
-    echo $deployment_progress_output | jq .
+    echo "Deployment has reached end state: $status"
+    echo "$deployment_progress_output" | jq .
 
     if [[ "$status" == "DEPLOYED" ]]; then
       exit 0
@@ -49,5 +48,4 @@ while [[ $counter -lt 100 ]] && [[ "$status" == "SCHEDULED" || "$status" == "DEP
   fi
 
   sleep 150
-
-done[/bash]
+done
